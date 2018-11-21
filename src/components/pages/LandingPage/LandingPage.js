cdcb4e2a78df1ab0aefe7583f2d9d97292f2a41b
howardcsps <howardcsps@gmail.com>
2018-11-20 20:57:01 -0800

    Changed font color, size, and style.

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './landingpage.css'
 class LandingPage extends Component {
   render() {
     return (
 <div>
       <h1 className='title'> Avail! </h1>
-=======
-class Test extends Component {
-  render() {
-    return (
-<div>
-      <h1> Avail! </h1>
->>>>>>> b6db04bcab6149924e66cba5daeb8c3e6c3e30ee
-
-
-      </div>
+       </div>
     )
   }
 }
-<<<<<<< HEAD
 export default LandingPage;
-=======
-export default Test;
->>>>>>> b6db04bcab6149924e66cba5daeb8c3e6c3e30ee
