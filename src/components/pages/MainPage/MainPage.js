d304c16fee80b56024f43a0368b5c9c34dc2aac5
howardcsps <howardcsps@gmail.com>
2018-11-20 21:29:34 -0800

    Added Main Page to "pages" folder

---
+++ src/components/pages/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './mainpage.css'
+class MainPage extends Component {
+  render() {
+    return (
+<div>
+      <h1 className='title'>Welcome To Avail!</h1>
+       </div>
+    )
+  }
+}
+export default MainPage;
